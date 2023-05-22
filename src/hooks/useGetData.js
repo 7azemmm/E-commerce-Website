da191e277f1c264add41acdfbb89633db3af2d57
hazem mohamed <hazemm7622@gmail.com>
2023-05-22 19:57:43 +0200

    comment on function on hooks

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,6 +1,8 @@
+// Importing the baseUrl from the '../Api/baseURL' module
 import baseUrl from '../Api/baseURL'
 
 const useGetData = async (url, parmas) => {
+    // Making a POST request to the server using baseUrl.post
     const res = await baseUrl.get(url, parmas);
     return res.data;
 }
