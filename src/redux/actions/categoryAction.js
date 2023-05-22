ab0e9c9f31cf0ab8d731f8554f7ed494f75b0da0
hazem mohamed <hazemm7622@gmail.com>
2023-05-22 20:09:02 +0200

    Action fetching all categories with pagination

--- src/redux/actions/categoryAction.js
+++ src/redux/actions/categoryAction.js
@@ -23,4 +23,25 @@ export const getAllCategory = (limit) => async (dispatch) => {
   }
 }
 
+// Action creator for fetching all categories with pagination
+export const getAllCategoryPage = (page) => async (dispatch) => {
+    try {
+      // Call the useGetData hook to fetch data from the categories API endpoint with the specified page and limit
+      const response = await useGetData(`/api/v1/categories?limit=6&page=${page}`);
+  
+      // Dispatch the GET_ALL_CATEGORY action with the fetched data as the payload
+      dispatch({
+        type: GET_ALL_CATEGORY,
+        payload: response,
+      })
+    } catch (e) {
+      // Dispatch the GET_ERROR action with the error message as the payload if there's an error
+      dispatch({
+        type: GET_ERROR,
+        payload: "Error " + e,
+      })
+    }
+  }
+  
+
 
