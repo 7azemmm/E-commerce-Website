f9979df8c187a4dd00325e1c83168e82cb57f783
hazem mohamed <hazemm7622@gmail.com>
2023-05-23 22:47:28 +0200

    create categories

--- server/services/categoryService.js
+++ server/services/categoryService.js
@@ -1,16 +1,16 @@
 const categoryModel= require('../models/categoryModel');
-
+const slugify= require('slugify');
 
 exports.getCategories=(req,res)=>{
 
-    const name=req.body.name;
-    console.log(req.body);
+    res.send();
+    }
+
+    exports.createCategories=(req,res)=>{
+        const name=req.body.name;
+        categoryModel.create({ name , slug:slugify(name)})
     
-    const newcategory= new categoryModel({name});
-    newcategory.save().then((doc)=>{
-        res.json(doc);
-    })
-    .catch((err)=>{
-        res.json(err); // callback function for the error and can be readable as a response
-    });
-};
+        .then((category) => res.status(201).json({data: category}))
+    
+        .catch((err) => res.status(400).send(err));
+    };
