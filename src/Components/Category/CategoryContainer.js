52d8291d5c62a03c0c6e6ddde873df461fcba241
abdelrahman2102577 <abdelrahman2102577@miuegypt.edu.eg>
2023-05-27 06:01:18 +0300

    categories

--- src/Components/Category/CategoryContainer.js
+++ src/Components/Category/CategoryContainer.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import React, { useEffect } from 'react'
+import { Container, Row, Spinner } from 'react-bootstrap'
 import CategoryCard from './../Category/CategoryCard';
-import short from "../../images/shorts4.png";
-import tshirt from "../../images/t-shirts16.png";
-import jeans from "../../images/jean11.png";
-import shirt from "../../images/shirt7.png";
-import hodies from "../../images/hodies.png";
-const CategoryContainer = () => {
+import { useSelector, useDispatch } from 'react-redux'
+import { getAllCategory } from '../../redux/actions/categoryAction'
+
+const CategoryContainer = ({ data ,loading}) => {
+
+    const colors = ["#FFD3E8", "#F4DBA5", "#55CFDF", "#FF6262", "#0034FF", "#FFD3E8"]
     return (
         <Container >
-        <div className="admin-content-text mt-2 ">كل التصنيفات</div>
+            <div className="admin-content-text mt-2 ">كل التصنيفات</div>
             <Row className='my-2 d-flex justify-content-between'>
-            <CategoryCard title=" شورت" img={short} background="#F4DBA4" />
-                <CategoryCard title="تيشيرت " img={tshirt} background="#F4DBA4" />
-                <CategoryCard title=" جينز" img={jeans} background="#F4DBA4" />
-                <CategoryCard title="شيرت " img={shirt} background="#F4DBA4" />
-                <CategoryCard title=" هودي" img={hodies} background="#F4DBA4" />
-                
+
+                {
+                    loading === false ? (
+                        data ? (
+                            data.map((item, index) => {
+                                return (<CategoryCard key={index} title={item.name} img={item.image} background={colors[Math.floor(Math.random() * 5) + 1]} />)
+                            })
+                        ) : <h4>لا يوجد تصنيفات</h4>
+                    ) : <Spinner animation="border" variant="primary" />
+
+                }
+
             </Row>
         </Container>
     )
 }
 
-export default CategoryContainer
+export default CategoryContainer 
