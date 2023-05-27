f16d2556f53de0c05632d8bbe8a2cb762b66ee60
hazem mohamed <hazemm7622@gmail.com>
2023-05-27 16:25:12 +0200

    Fetch all brands

---
+++ src/redux/actions/brandAction.js
@@ -0,0 +1,25 @@
+import { GET_ALL_BRAND, GET_ERROR, CREATE_BRAND } from '../type'
+import useGetData from '../../hooks/useGetData'
+import { useInsertDataWithImage } from '../../hooks/useInsertData'
+
+// Action creator: Fetch all brands
+export const getAllBrand = (limit) => async (dispatch) => {
+  try {
+    // Call the custom hook useGetData to fetch all brands with the specified limit
+    const response = await useGetData(`/api/v1/brands?limit=${limit}`);
+
+    // Dispatch the fetched brands to the Redux store using the GET_ALL_BRAND type
+    dispatch({
+      type: GET_ALL_BRAND,
+      payload: response,
+    })
+  } catch (e) {
+    // Dispatch an error action if an error occurs during the API request
+    dispatch({
+      type: GET_ERROR,
+      payload: "Error " + e,
+    })
+  }
+}
+
+
