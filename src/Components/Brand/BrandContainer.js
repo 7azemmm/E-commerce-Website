83b816f0e42a492a174d87a06a00fcb468848cf1
abdelrahman2102577 <abdelrahman2102577@miuegypt.edu.eg>
2023-05-27 06:28:16 +0300

    brand container

--- src/Components/Brand/BrandContainer.js
+++ src/Components/Brand/BrandContainer.js
@@ -1,36 +1,26 @@
 import React from 'react'
 import BrandCard from './BrandCard'
-import brand1 from "../../images/zara.png";
-import brand2 from "../../images/puma.png";
-import brand3 from "../../images/nike.png";
-import brand4 from "../../images/adidas.png";
-import brand5 from "../../images/gucci.png";
-import brand6 from "../../images/armani.png";
-import brand7 from "../../images/hm.png";
-import brand8 from "../../images/glamour.png";
-import brand9 from "../../images/fashion.png";
-import brand10 from "../../images/hrren.png";
-import brand11 from "../../images/hollister.png";
-import brand12 from "../../images/lorem.png";
-import { Container, Row } from 'react-bootstrap';
-const BrandContainer = () => {
+import brand1 from "../../images/brand1.png";
+import brand2 from "../../images/brand2.png";
+import brand3 from "../../images/brand3.png";
+import { Container, Row, Spinner } from 'react-bootstrap';
+
+const BrandContainer = ({ data,loading }) => {
+   
     return (
         <Container>
             <div className="admin-content-text mt-2 ">كل الماركات</div>
             <Row className='my-1 d-flex justify-content-between'>
-                <BrandCard img={brand1} />
-                <BrandCard img={brand2} />
-                <BrandCard img={brand3} />
-                <BrandCard img={brand4} />
-                <BrandCard img={brand5} />
-                <BrandCard img={brand6} />
-                <BrandCard img={brand7} />
-                <BrandCard img={brand8} />
-                <BrandCard img={brand9} />
-                <BrandCard img={brand10} />
-                <BrandCard img={brand11} />
-                <BrandCard img={brand12} />
-             
+
+                {
+                    loading === false ? (
+                        data ? (
+                            data.map((item, index) => {
+                                return (<BrandCard key={index} img={item.image} />)
+                            })
+                        ) : <h4>لا يوجد ماركات</h4>
+                    ) : <Spinner animation="border" variant="primary" />
+                }
 
             </Row>
         </Container>
