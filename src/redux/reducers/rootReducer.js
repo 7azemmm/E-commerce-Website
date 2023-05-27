6cce40513fb200051c01687ea291756c21a79d55
abdelrahman2102577 <abdelrahman2102577@miuegypt.edu.eg>
2023-05-27 16:51:54 +0300

    root reucer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,7 +1,8 @@
 import {combineReducers} from 'redux'//imports the combineReducers function from the Redux library.
 import categoryReducer from './categoryReducer'//imports the categoryReducer from the categoryReducer.js file 
-
+import brandReducer from './brandReducer'//imports the categoryReducer from the brandReducer.js file 
 export default combineReducers ({//exports a default function
     allCategory:categoryReducer ,//the object has  allCategory(property) which represents a piece of state managed by the categoryReducer.
+    allBrand: brandReducer
 }) 
 ///root reducer
