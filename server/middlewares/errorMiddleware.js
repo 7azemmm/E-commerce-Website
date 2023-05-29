623d4907ab634441bd356be9f482006f0249e6ee
karim-7423 <karim2110342@miuegypt.edu.eg>
2023-05-30 01:06:59 +0300

    modify middlware for handling error in DeV andProd

---
+++ server/middlewares/errorMiddleware.js
@@ -0,0 +1,35 @@
+const ApiError = require('../utils/apiError');//require APIerror class that deal with expected errors
+
+const sendErrorForDev = (err, res) => //this function send the error in details as it is  important in development mode
+  res.status(err.statusCode).json({
+    status: err.status,//the error status
+    error: err,
+    message: err.message,//the error message
+    stack: err.stack,//where is the error
+  });
+
+const sendErrorForProd = (err, res) => //send the error with only status code and message as we dont need details in production mode
+  res.status(err.statusCode).json({ // i need only statuse code and error message
+    status: err.status,
+    message: err.message,
+  });
+
+const handleJwtInvalidSignature = () =>
+  new ApiError('Invalid token, please login again..', 401);
+
+const handleJwtExpired = () =>
+  new ApiError('Expired token, please login again..', 401);
+
+const globalError = (err, req, res, next) => {//global error middleware that handles express errors 
+  err.statusCode = err.statusCode || 500; //if it has status code ok else it's 500
+  err.status = err.status || 'error';//if it has error status ok else it is  "error" 
+  if (process.env.NODE_ENV === 'development') { //check if we are in the devolpment mode use this function
+    sendErrorForDev(err, res);
+  } else {
+    if (err.name === 'JsonWebTokenError') err = handleJwtInvalidSignature();
+    if (err.name === 'TokenExpiredError') err = handleJwtExpired();
+    sendErrorForProd(err, res);//calling this function as we are in production mode
+  }
+};
+
+module.exports = globalError;
