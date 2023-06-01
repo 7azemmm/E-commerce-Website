b801c164532265dd7f80ae665a5226d8cab35a5b
karim-7423 <karim2110342@miuegypt.edu.eg>
2023-06-02 00:00:46 +0300

    brand action updated

--- src/redux/actions/brandAction.js
+++ src/redux/actions/brandAction.js
@@ -1,68 +1,75 @@
-import { GET_ALL_BRAND, GET_ERROR, CREATE_BRAND } from '../type'
+import { GET_ALL_BRAND, GET_ONE_BRAND, GET_ERROR, CREATE_BRAND } from '../type'
 import useGetData from '../../hooks/useGetData'
 import { useInsertDataWithImage } from '../../hooks/useInsertData'
 
-// Action creator: Fetch all brands
+//get all Brand
 export const getAllBrand = (limit) => async (dispatch) => {
-  try {
-    // Call the custom hook useGetData to fetch all brands with the specified limit
-    const response = await useGetData(`/api/v1/brands?limit=${limit}`);
+    try {
+        const response = await useGetData(`/api/v1/brands?limit=${limit}`);
 
-    // Dispatch the fetched brands to the Redux store using the GET_ALL_BRAND type
-    dispatch({
-      type: GET_ALL_BRAND,
-      payload: response,
-    })
-  } catch (e) {
-    // Dispatch an error action if an error occurs during the API request
-    dispatch({
-      type: GET_ERROR,
-      payload: "Error " + e,
-    })
-  }
+        dispatch({
+            type: GET_ALL_BRAND,
+            payload: response,
+        })
+
+    } catch (e) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error " + e,
+        })
+    }
 }
 
-// Action creator: Fetch brands with pagination
+//get one Brand
+export const getOneBrand = (id) => async (dispatch) => {
+    try {
+        const response = await useGetData(`/api/v1/brands/${id}`);
+
+        dispatch({
+            type: GET_ONE_BRAND,
+            payload: response,
+        })
+
+    } catch (e) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error " + e,
+        })
+    }
+}
+
+//get all Brand with pagination
 export const getAllBrandPage = (page) => async (dispatch) => {
-  try {
-    const response = await useGetData(`/api/v1/brands?limit=4&page=${page}`);
+    try {
+        const response = await useGetData(`/api/v1/brands?limit=4&page=${page}`);
+        dispatch({
+            type: GET_ALL_BRAND,
+            payload: response,
+        })
 
-    // Dispatch the fetched brands to the Redux store using the GET_ALL_BRAND type
-    dispatch({
-      type: GET_ALL_BRAND,
-      payload: response,
-    })
-  } catch (e) {
-    
-    dispatch({
-      type: GET_ERROR,
-      payload: "Error " + e,
-    })
-  }
+    } catch (e) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error " + e,
+        })
+    }
 }
 
-// Action creator: Create a new brand
+
+//insert brand with pagination
 export const createBrand = (formData) => async (dispatch) => {
-  try {
-    // Call the custom hook useInsertDataWithImage to send the formData and create a new brand
-    const response = await useInsertDataWithImage(`/api/v1/brands`, formData);
+    try {
+        const response = await useInsertDataWithImage(`/api/v1/brands`, formData);
+        dispatch({
+            type: CREATE_BRAND,
+            payload: response,
+            loading: true
+        })
 
-    // Dispatch the created brand to the Redux store using the CREATE_BRAND type
-    dispatch({
-      type: CREATE_BRAND,
-      payload: response,
-      loading: true
-    })
-  } catch (e) {
-    // Dispatch an error action if an error occurs during the API request
-    dispatch({
-      type: GET_ERROR,
-      payload: "Error " + e,
-    })
-  }
+    } catch (e) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error " + e,
+        })
+    }
 }
-
-
-
-
-
