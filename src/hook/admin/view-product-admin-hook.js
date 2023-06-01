e94c1eed17a5d88ea9b807faa57d104cea59bd22
Test-073 <test27760@gmail.com>
2023-06-02 00:18:33 +0300

    create func fires when clicking product (Admin)

--- src/hook/admin/view-product-admin-hook.js
+++ src/hook/admin/view-product-admin-hook.js
@@ -11,3 +11,11 @@ const ViewProductAdminHook = () => {
         dispatch(getAllProducts(8))
     }, [])
 
+
+//onPress 
+const onPress = async (page) => {
+    await dispatch(getAllProductsPage(page, 8))
+}
+let items = []; let pagination = [];
+const allProducts = useSelector((state) => state.allproducts.allProducts)
+
