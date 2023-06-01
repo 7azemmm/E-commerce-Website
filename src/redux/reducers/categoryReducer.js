206b334add3f0d293ffe3b6ff3413ea5f2dc2211
karim-7423 <karim2110342@miuegypt.edu.eg>
2023-06-01 23:59:57 +0300

    categoryreducer updated

--- src/redux/reducers/categoryReducer.js
+++ src/redux/reducers/categoryReducer.js
@@ -1,39 +1,35 @@
+import { GET_ALL_CATEGORY, GET_ONE_CATEGORY, GET_ERROR, CREATE_CATEGORY } from '../type'
 
-// Importing action type constants
-import { GET_ALL_CATEGORY, GET_ERROR, CREATE_CATEGORY } from '../type'
-
-// Initial state for the category reducer
-const initial = {
-  category: [],
-  loading: true,
+const inital = {
+    category: [],
+    oneCategory: [],
+    loading: true,
 }
-
-// Category reducer function
-const categoryReducer = (state = initial, action) => {
-  switch (action.type) {
-    case GET_ALL_CATEGORY:
-      // Updating state when the GET_ALL_CATEGORY action is dispatched
-      return {
-        ...state, //helps to create a new state object that incorporates the previous state's properties and values
-        category: action.payload,
-        loading: false,
-      }
-    case CREATE_CATEGORY:
-      // Updating state when the CREATE_CATEGORY action is dispatched
-      return {
-        category: action.payload,
-        loading: false,
-      }
-    case GET_ERROR:
-      // Updating state when the GET_ERROR action is dispatched
-      return {
-        loading: true,
-        category: action.payload,
-      }
-    default:
-      // Returning the current state for any other action
-      return state;
-  }
+const categoryReducer = (state = inital, action) => {
+    switch (action.type) {
+        case GET_ALL_CATEGORY:
+            return {
+                ...state,
+                category: action.payload,
+                loading: false,
+            }
+        case GET_ONE_CATEGORY:
+            return {
+                oneCategory: action.payload,
+                loading: false,
+            }
+        case CREATE_CATEGORY:
+            return {
+                category: action.payload,
+                loading: false
+            }
+        case GET_ERROR:
+            return {
+                loading: true,
+                category: action.payload,
+            }
+        default:
+            return state;
+    }
 }
-
-export default categoryReducer;
+export default categoryReducer
