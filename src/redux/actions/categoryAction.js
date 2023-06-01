88a59da0d924710c22e77c2abb1bd7b128d44c66
abdelrahman2102577 <abdelrahman2102577@miuegypt.edu.eg>
2023-06-01 22:21:16 +0300

    category action

--- src/redux/actions/categoryAction.js
+++ src/redux/actions/categoryAction.js
@@ -1,69 +1,74 @@
-// Importing action type constants and custom hooks
-import { GET_ALL_CATEGORY, GET_ERROR, CREATE_CATEGORY } from '../type'
+import { GET_ALL_CATEGORY, GET_ERROR,GET_ONE_CATEGORY, CREATE_CATEGORY } from '../type'
 import useGetData from '../../hooks/useGetData'
 import { useInsertDataWithImage } from '../../hooks/useInsertData'
-
-// Action creator for fetching all categories with a specified limit
+//get all category
 export const getAllCategory = (limit) => async (dispatch) => {
-  try {
-    // Call the useGetData hook to fetch data from the categories API endpoint with the specified limit
-    const response = await useGetData(`/api/v1/categories?limit=${limit}`);
+    try {
+        const response = await useGetData(`/api/v1/categories?limit=${limit}`);
 
-    // Dispatch the GET_ALL_CATEGORY action with the fetched data as the payload
-    dispatch({
-      type: GET_ALL_CATEGORY,
-      payload: response,
-    })
-  } catch (e) {
-    // Dispatch the GET_ERROR action with the error message as the payload if there's an error
-    dispatch({
-      type: GET_ERROR,
-      payload: "Error " + e,
-    })
-  }
+        dispatch({
+            type: GET_ALL_CATEGORY,
+            payload: response,
+        })
+
+    } catch (e) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error " + e,
+        })
+    }
 }
 
-// Action creator for fetching all categories with pagination
+//get one category with
+export const getOneCategory = (id) => async (dispatch) => {
+    try {
+        const response = await useGetData(`/api/v1/categories/${id}`);
+
+        dispatch({
+            type: GET_ONE_CATEGORY,
+            payload: response,
+        })
+
+    } catch (e) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error " + e,
+        })
+    }
+}
+
+//get all category with pagination
 export const getAllCategoryPage = (page) => async (dispatch) => {
     try {
-      // Call the useGetData hook to fetch data from the categories API endpoint with the specified page and limit
-      const response = await useGetData(`/api/v1/categories?limit=6&page=${page}`); // limit is assigned to a value of 6 and page parameter is used for pagination.
-  
-      // Dispatch the GET_ALL_CATEGORY action with the fetched data as the payload
-      dispatch({
-        type: GET_ALL_CATEGORY,
-        payload: response,
-      })
-    } catch (e) {
-      // Dispatch the GET_ERROR action with the error message as the payload if there's an error
-      dispatch({
-        type: GET_ERROR,
-        payload: "Error " + e,
-      })
-    }
-  }
+        const response = await useGetData(`/api/v1/categories?limit=6&page=${page}`);
+        dispatch({
+            type: GET_ALL_CATEGORY,
+            payload: response,
+        })
 
-  // Action creator for creating a new category
+    } catch (e) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error " + e,
+        })
+    }
+}
+
+
+//get all category with pagination
 export const createCategory = (formData) => async (dispatch) => {
     try {
-      // Call the useInsertDataWithImage hook to send a POST request to the categories API endpoint with the provided formData
-      const response = await useInsertDataWithImage(`/api/v1/categories`, formData);
-  
-      // Dispatch the CREATE_CATEGORY action with the response as the payload and set loading to true
-      dispatch({
-        type: CREATE_CATEGORY,
-        payload: response,
-        loading: true
-      })
+        const response = await useInsertDataWithImage(`/api/v1/categories`, formData);
+        dispatch({
+            type: CREATE_CATEGORY,
+            payload: response,
+            loading: true
+        })
+
     } catch (e) {
-      // Dispatch the GET_ERROR action with the error message as the payload if there's an error
-      dispatch({
-        type: GET_ERROR,
-        payload: "Error " + e,
-      })
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error " + e,
+        })
     }
-  }
-
-  
-
-
+}
