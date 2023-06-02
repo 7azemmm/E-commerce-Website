71966ad62e1f6c582c2dae1ff665707d449a9a2a
abdelrahman2102577 <abdelrahman2102577@miuegypt.edu.eg>
2023-06-03 02:25:22 +0300

    nav bar login

--- src/Components/Uitily/NavBarLogin.js
+++ src/Components/Uitily/NavBarLogin.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import { Navbar, Container, FormControl, Nav } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Navbar, Container, FormControl, Nav, NavDropdown } from 'react-bootstrap'
 import logo from '../../images/logo.png'
 import login from '../../images/login.png'
 import cart from '../../images/cart.png'
 import NavbarSearchHook from './../../hook/search/navbar-search-hook';
+import { useDispatch, useSelector } from 'react-redux';
+import { getLoggedUser } from './../../redux/actions/authAction';
+
 const NavBarLogin = () => {
+
     const [OnChangeSearch, searchWord] = NavbarSearchHook()
     let word = "";
     if (localStorage.getItem("searchWord") != null)
         word = localStorage.getItem("searchWord")
+
+    const [user, setUser] = useState('');
+    useEffect(() => {
+        if (localStorage.getItem("user") != null)
+            setUser(JSON.parse(localStorage.getItem("user")))
+    }, [])
+
+    const logOut = () => {
+        localStorage.removeItem("user")
+        setUser('')
+    }
+
     return (
         <Navbar className="sticky-top" bg="dark" variant="dark" expand="sm">
             <Container>
@@ -28,11 +44,26 @@ const NavBarLogin = () => {
                         aria-label="Search"
                     />
                     <Nav className="me-auto">
-                        <Nav.Link href='/login'
-                            className="nav-text d-flex mt-3 justify-content-center">
-                            <img src={login} className="login-img" alt="sfvs" />
-                            <p style={{ color: "white" }}>دخول</p>
-                        </Nav.Link>
+                        {
+                            user != '' ? (
+                                <NavDropdown title={user.name} id="basic-nav-dropdown">
+
+
+                                    {
+                                        user.role === "admin" ? (<NavDropdown.Item href="/admin/allproducts">لوحة التحكم</NavDropdown.Item>) : (<NavDropdown.Item href="/user/profile">الصفحه الشخصية</NavDropdown.Item>)
+                                    }
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item onClick={logOut} href="/">تسجيل خروج</NavDropdown.Item>
+
+                                </NavDropdown>
+                            ) :
+                                (<Nav.Link href='/login'
+                                    className="nav-text d-flex mt-3 justify-content-center">
+                                    <img src={login} className="login-img" alt="sfvs" />
+                                    <p style={{ color: "white" }}>دخول</p>
+                                </Nav.Link>)
+                        }
+
                         <Nav.Link href='/cart'
                             className="nav-text d-flex mt-3 justify-content-center"
                             style={{ color: "white" }}>
@@ -45,5 +76,6 @@ const NavBarLogin = () => {
         </Navbar>
     )
 }
+
 
 export default NavBarLogin
