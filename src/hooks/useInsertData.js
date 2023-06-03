04553e62c2f6d1c1bd6d319ff30ef72691702197
abdelrahman2102577 <abdelrahman2102577@miuegypt.edu.eg>
2023-06-03 03:06:08 +0300

    user delete and insert and update data

--- src/hooks/useInsertData.js
+++ src/hooks/useInsertData.js
@@ -1,32 +1,27 @@
-// Importing the baseUrl from the '../Api/baseURL' module
 import baseUrl from '../Api/baseURL'
 
-// Function for inserting data with an image 
-const useInsertDataWithImage = async (url, params) => { 
-  // Configuration object with headers for multipart/form-data which is used to upload files
-  const config = {
-    headers: { "Content-Type": "multipart/form-data" }
-  };
 
-  // Making a POST request to the server using baseUrl.post
-  const res = await baseUrl.post(url, params, config);
-
-  // Logging the status of the response to the console
-  console.log(res.status);
-
-  // Returning the response object
-  return res;
+const useInsertDataWithImage = async (url, parmas) => {
+    const config = {
+        headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+    }
+    const res = await baseUrl.post(url, parmas, config);
+    return res;
 }
 
-// Function for inserting data without an image
-const useInsertData = async (url, params) => {
-  // Making a POST request to the server using baseUrl.post
-  const res = await baseUrl.post(url, params);
 
-  // Returning the response object
-  return res;
+const useInsertData = async (url, parmas) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+    }
+    const res = await baseUrl.post(url, parmas, config);
+    return res;
 }
 
-// Exporting the useInsertData and useInsertDataWithImage functions
+
 export { useInsertData, useInsertDataWithImage };
-
