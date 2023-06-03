0878c629e2b5b4c4bb8e72f26b4b4199884e6bcd
karim-7423 <karim2110342@miuegypt.edu.eg>
2023-06-03 18:15:41 +0300

    productroute updated to upload images

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -8,16 +8,20 @@ const{getProducts,
     createProduct,
     updateProduct,
      getProduct,
-     deleteProduct}=require('../services/productService');
+     deleteProduct,
+     uploadProductImages,
+     resizeProductImages}=require('../services/productService');
 
 const router=express.Router();
 
 
 router.route('/').get(getProducts);
-router.route('/').post(createProductValidator,createProduct);
+router.route('/').post(uploadProductImages,
+    resizeProductImages,createProductValidator,createProduct);
 // first add rules then the middleware to catch error then if there is no error in the request it will pass to getCategory Service 
 router.route('/:id').get(getProductValidator,getProduct);
-router.route('/:id').put(updateProductValidator,updateProduct).delete(deleteProductValidator,deleteProduct);
+router.route('/:id').put(uploadProductImages,
+    resizeProductImages,updateProductValidator,updateProduct).delete(deleteProductValidator,deleteProduct);
 
 
 
