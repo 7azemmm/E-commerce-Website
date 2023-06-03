8f310c2c751caefce8f6e5fe63e705ac90280bf8
karim-7423 <karim2110342@miuegypt.edu.eg>
2023-06-03 18:10:12 +0300

    categoryservice updated to upload image

--- server/routes/categoryRoute.js
+++ server/routes/categoryRoute.js
@@ -2,7 +2,9 @@ const express=require('express');
 
 const{getCategories,
      getCategory,
-     createCategories
+     createCategories,
+     uploadCategoryImage,
+  resizeImage,
     }=require('../services/categoryService')
 
     const{
@@ -15,7 +17,8 @@ const router=express.Router();
 const subCategoryRoute= require('./subCategoryRoute');
 
 router.use('/:categoryId/subcategories',subCategoryRoute);
-router.route('/').get(getCategories).post(createCategoryValidator,createCategories);
+router.route('/').get(getCategories).post(uploadCategoryImage,
+    resizeImage,createCategoryValidator,createCategories);
 router.route('/:id').get(getCategoryValidator,getCategory);
 
 module.exports=router;
