f008c71aff17b8d3fa8210bb8160ddc586b75d9e
karim-7423 <karim2110342@miuegypt.edu.eg>
2023-06-03 17:59:53 +0300

    brandsModel updated

--- server/models/brandsModel.js
+++ server/models/brandsModel.js
@@ -18,6 +18,21 @@ const Brands= new mongoose.Schema({
 
     },{timestamps:true} 
     );
+    const setImageURL = (doc) => {
+      if (doc.image) {
+        const imageUrl = `${process.env.BASE_URL}/brands/${doc.image}`;
+        doc.image = imageUrl;
+      }
+    };
+    // findOne, findAll and update
+    brandSchema.post('init', (doc) => {
+      setImageURL(doc);
+    });
+    
+    // create
+    brandSchema.post('save', (doc) => {
+      setImageURL(doc);
+    });
     
     // change schema to model
     const BrandsModel= mongoose.model('BrandsModel',Brands);
